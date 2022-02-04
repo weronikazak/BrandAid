09f9a27514f4acce415a90ca933afb951d0ccc85
Michael Engelhardt <michael.engelhardt.s6@gmail.com>
2022-02-04 12:03:38 +0100

    Remove URL prefix and basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-	images: {
-		domains: ["cloudflare-ipfs.com", "etherscan.io"],
-	},
-	assetPrefix: "/ethereum-nextjs-boilerplate/",
-	basePath: "/ethereum-nextjs-boilerplate",
+  images: {
+    domains: ["cloudflare-ipfs.com", "etherscan.io"],
+  },
 };
